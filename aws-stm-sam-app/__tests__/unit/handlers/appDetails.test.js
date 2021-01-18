21469ddc35efb79f29bbdebcb6c30f045a427a95
OlliePalmer <palmerollie@gmail.com>
2021-01-18 18:52:33 +0000

    Added tests for helpers.js

--- aws-stm-sam-app/__tests__/unit/handlers/appDetails.test.js
+++ aws-stm-sam-app/__tests__/unit/handlers/appDetails.test.js
@@ -1,5 +1,6 @@
 const appDetailsLambda = require('../../../src/handlers/appDetails.js');
-const { inspect } = require('../../../src/helpers.js');
+// const { inspect } = require('../../../src/helpers.js');
+const helpers = require('../../../src/helpers.js');
 
 // This includes all tests for appDetails handler
 describe('Test for appDetails handler', function () {
@@ -21,6 +22,26 @@ describe('Test for appDetails handler', function () {
         expect(resultDevelopers).toEqual(expectedDevelopers);
     });
 
+    
+
 
    
+});
+
+
+
+describe('Test for helpers.js that appDetails relies upon', function () {
+    it('Verifies json retrieved from source', async () => {
+        const testAppId = 212680;
+        const steamUrlAppDetailsEndpoint = (appId) => { return `https://store.steampowered.com/api/appdetails?appids=${appId}&purchase_type=all&language=all`; }
+        const json = await helpers.getJsonFromRequest(steamUrlAppDetailsEndpoint(testAppId));
+
+        //Request to retrieve JSON was a success
+        expect(json[testAppId].success).toEqual(true);
+
+        //Data is as expected
+        const expectedName = 'FTL: Faster Than Light'; 
+        const resultName = json[testAppId].data.name;
+        expect(resultName).toEqual(expectedName);
+    });
 });
